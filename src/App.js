370e47b3d4afcb39e46967ed7d3fdecb54b28657
evelyn <evelyn.chin179@gmail.com>
2020-11-07 14:32:51 -0500

    basic mvp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: "",
+      last: "",
+      number: 0,
+      region: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert ("Hey there, " + this.state.first + ". Thanks for signing up. You've just taken a step towards creating a greener future.");
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          First: 
+          <input type="text" name="first" value={this.state.first} onChange={this.handleInputChange}/>
+        </label> 
+        <label>
+          Last: 
+          <input type="text" name="last" value={this.state.last} onChange={this.handleInputChange}/>
+        </label> 
+        <label>
+          Number: 
+          <input type="number" name="number" value={this.state.number} onChange={this.handleInputChange}/>
+        </label> 
+        <label>
+          State: 
+          <input type="text" name="region" value={this.state.region} onChange={this.handleInputChange}/>
+        </label> 
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   <User />
+// }
+
+export default User;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import User from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<User />, document.getElementById('root'));
